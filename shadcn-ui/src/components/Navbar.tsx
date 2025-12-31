e5ae21f4f3f00cf8eb4256894133726e2afc1c05
MGX <admin@mgx.dev>
2025-12-31 02:36:33 +0000

    Logo updated and stretched.

--- shadcn-ui/src/components/Navbar.tsx
+++ shadcn-ui/src/components/Navbar.tsx
@@ -15,14 +15,14 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 border-b border-gray-800 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          {/* Left: Logo */}
+        <div className="flex items-center justify-between h-24">
+          {/* Left: Logo - Stretched to fill header as much as possible */}
           <div className="flex-shrink-0">
             <Link to="/" className="flex items-center">
               <img 
                 src="/fanhug-logo.png" 
                 alt="FanHug Logo" 
-                className="h-14"
+                className="h-20 w-auto"
               />
             </Link>
           </div>
